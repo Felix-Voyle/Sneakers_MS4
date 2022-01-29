58b4e4d6ef6e80f3ac580e3a2ff8beeb90a25e11
Felix-Voyle <felix.voyle@icloud.com>
2022-01-29 16:26:56 +0000

    brand dropdown populates from brand model

--- products/models.py
+++ products/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 
 class Brand(models.Model):
     name = models.CharField(max_length=254)
@@ -12,16 +10,17 @@ class Brand(models.Model):
     
     def get_friendly_name(self):
         return self.friendly_name
-    
+
+
 class Product(models.Model):
     brand = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    rating=models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
     release_date = models.CharField(max_length=12, null=True, blank=False)
-    image=models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Brand
 
 # Create your views here.
 
@@ -8,9 +8,11 @@ def all_products(request):
     """A view to show products that have released and available to buy"""
 
     products = Product.objects.all()
+    brands = Brand.objects.all()
 
     context = {
         'products': products,
+        'brands': brands,
     }
 
     return render(request, 'products/products.html', context)
@@ -20,9 +22,11 @@ def product_detail(request, product_id):
     """A view to show product details of individual product"""
 
     product = get_object_or_404(Product, pk=product_id)
+    brands = Brand.objects.all()
 
     context = {
         'product': product,
+        'brands': brands,
     }
 
     return render(request, 'products/product_detail.html', context)
