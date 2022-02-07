907a3c840419a9647b4160130ac8779188521fe5
Felix-Voyle <felix.voyle@icloud.com>
2022-02-07 21:19:08 +0000

    added stripe card elemnt

--- checkout/views.py
+++ checkout/views.py
@@ -5,15 +5,18 @@ from .forms import OrderForm
 
 
 def checkout(request):
+    """checkout"""
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag")
         return redirect(reverse('products'))
-    
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KEEvlCpvTq3sgPLIq3jcJn7fiRfMqkJQ9GXFK74wrHeyOM9hKQAmEdBfAixFq2S4Q9mr8xz2Q9CIZJzVAKAV9h100v9VcrKWY',
+        'client_secret': 'test secret key',
     }
 
     return render(request, template, context)
