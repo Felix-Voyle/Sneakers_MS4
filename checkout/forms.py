7060347385fe8be5d3ce6ea7fe094dd68213920e
Felix-Voyle <felix.voyle@icloud.com>
2022-02-12 15:13:13 +0000

    updated checkout form countries

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,7 +3,9 @@ from .models import Order
 
 
 class OrderForm(forms.ModelForm):
+    """Checkout order form"""
     class Meta:
+        """Order fields"""
         model = Order
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
