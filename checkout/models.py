969196dd47c3146f4dbff1e40bf3296215e57529
Felix-Voyle <felix.voyle@icloud.com>
2022-02-12 16:30:45 +0000

    Added profiles app

--- checkout/models.py
+++ checkout/models.py
@@ -7,12 +7,16 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from django.contrib.auth.models import User
+
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     """Order Model"""
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     user = models.ForeignKey(
         User, null=True, blank=True, on_delete=models.SET_NULL)
     full_name = models.CharField(max_length=50, null=False, blank=False)
