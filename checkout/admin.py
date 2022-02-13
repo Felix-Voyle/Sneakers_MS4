7d041cd992fae30a858e43bd627252f0f2df3cb5
Felix-Voyle <felix.voyle@icloud.com>
2022-02-13 08:18:33 +0000

    changed date location in upcoming cards, removed unnecessary order model field

--- checkout/admin.py
+++ checkout/admin.py
@@ -3,18 +3,20 @@ from .models import Order, OrderLineItem
 
 
 class OrderLineItemAdminInline(admin.TabularInline):
+    """Order Line Items"""
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
+    """Order Admin"""
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total', 'user',)
+                       'grand_total',)
 
-    fields = ('order_number', 'user', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
