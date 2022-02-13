1e38938b1ebee7cf41b7b501636be6a4a6a0a3de
Felix-Voyle <felix.voyle@icloud.com>
2022-02-13 12:11:03 +0000

    added product admin product form

---
+++ products/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Product, Brand
+
+
+class ProductForm(forms.ModelForm):
+    """Product Form"""
+    class Meta:
+        """generates all product fields"""
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        brands = Brand.objects.all()
+        freindly_names = [(b.id, b.get_friendly_name) for b in brands]
+
+        self.fields['brands'].choices = freindly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
