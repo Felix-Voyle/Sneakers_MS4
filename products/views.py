f04734df402dd75080a1ac60d8f9e92b9454f0ad
Felix-Voyle <felix.voyle@icloud.com>
2022-02-15 20:09:49 +0000

    added for loop in upcoming product detail to display all comments on relevant product

--- products/views.py
+++ products/views.py
@@ -103,10 +103,12 @@ def upcoming_product_detail(request, product_id):
 
     product = get_object_or_404(Product, pk=product_id)
     brands = Brand.objects.all()
+    comments = Comment.objects.filter(product=product)
 
     context = {
         'product': product,
         'brands': brands,
+        'comments': comments,
     }
 
     return render(request, 'products/upcoming_product_detail.html', context)
