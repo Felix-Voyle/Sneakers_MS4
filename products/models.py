dc6befbb3b0ea232caabafc4eeba69cd72f1899e
Felix-Voyle <felix.voyle@icloud.com>
2022-02-19 16:16:02 +0000

    added an add brand template and view to product management

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Brand(models.Model):
     """Brand Model"""
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, null=False, blank=False)
 
     def __str__(self):
         return self.name
